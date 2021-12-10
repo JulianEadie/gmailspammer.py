b60e0053ed49f881998ff5078f0ed723c578c8c9
JulianEadie <80416799+JulianEadie@users.noreply.github.com>
2021-12-10 14:02:36 -0500

    Update password.py

--- password.py
+++ password.py
@@ -22,6 +22,6 @@ passwordEntered.grid(column = 0, row = 1)
 
 
 button = ttk.Button(window, text = "Enter", command = Enterpassword)
-button.grid = (column = 0, row = 2)
+button.grid(column = 0, row = 2)
 
 window.mainloop()
