ca74f09755d9771c9f72810eb0d3e27c3cf9a22d
JulianEadie <80416799+JulianEadie@users.noreply.github.com>
2021-12-10 13:46:24 -0500

    Create receiver_email.py

---
+++ receiver_email.py
@@ -0,0 +1,27 @@
+import tkinter as tk 
+from tkinter import tkk
+
+
+window = tk.Tk()
+
+window.title("Python Tkinter Text Box")
+window.minsize(600, 400)
+
+def receiver_email():
+    label.configure(text = 'Hello' + receiver_email.get())
+    
+label = ttk.Label(window, text = "Enter the receivers email)
+label.grid(colomn = 0, row = 0)
+                  
+                  
+                  
+                  
+receiver_email = tk.StringVar()
+receiver_emailEntered = ttk.Entry(window, width = 15, textvariable = receiver_email)
+receiver_emailEntered.grid(column = 0, row = 1)
+                  
+
+button = ttk.Button(window, text = "Enter", command = Enterpassword)
+button.grid = (column = 0, row = 1)
+                  
+window.mainloop()                  
