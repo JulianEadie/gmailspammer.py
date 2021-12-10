f92b280e75e3cebe4399c26e0bcd9bb0c3233e10
JulianEadie <80416799+JulianEadie@users.noreply.github.com>
2021-12-10 10:44:21 -0500

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -4,4 +4,25 @@ import spammer
 def SendEmail(message)
     smtp_server = "smtp.gmail.com"
     port = "586"
+    sender_email = ""
+    password = ""
+    receiver_email""
+    
+    context = ssl.creat_default_context()
+    
+    try:
+        server = smtplib.SMTP(smtp_server, port)
+        server.ehlo
+        server.starttls(context=context)
+        server.ehlo
+        server.login(sender_email, password)
+        server.sendemail( sender_email, receiver_email, message)
+        
+    except Exception as e:
+        print(e)
+      finally:
+        server.quit()
+        
+text = text.txt
+sendemail(txt)
     
